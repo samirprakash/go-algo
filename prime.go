0e16e0a806ae4974d183326b17d0d5effb712926
Samir Prakash <Samir.Prakash@gmail.com>
2022-06-05 14:05:47 -0500

    optimized prime check algo

--- prime.go
+++ prime.go
@@ -1,17 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+	"math"
+)
 
 func main() {
-	log.Println("isPrime(5) => ", isPrime(5)) // [1*5 or 5*1] => true
-	log.Println("isPrime(4) => ", isPrime(4)) // [1*4 or 2*2 or 4*1] => false
+	log.Println("isPrime(5) => ", isPrime(5))   // [1*5 or 5*1] => true
+	log.Println("isPrime(4) => ", isPrime(4))   // [1*4 or 2*2 or 4*1] => false
+	log.Println("isPrime(13) => ", isPrime(13)) // [11*12 or 12*11] => true
 }
 
 func isPrime(n int) bool {
 	if n <= 1 {
 		return false
 	}
-	for i := 2; i < n; i++ {
+	nn := math.Sqrt(float64(n))
+	for i := 2; i <= int(nn); i++ {
 		if n%i == 0 {
 			return false
 		}
